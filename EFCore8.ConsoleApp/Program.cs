cb4aa04016c06b263e4ab5db186ccd5c29acd784
murcell <murselgurkan@gmail.com>
2024-08-10 16:37:29 +0300

    Capter nine

--- EFCore8.ConsoleApp/Program.cs
+++ EFCore8.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using EFCore8.ConsoleApp.Data;
+using EFCore8.ConsoleApp.Entities;
+using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Uygulama başladı!");
 
@@ -6,8 +8,56 @@ Console.WriteLine("Uygulama başladı!");
 //MgDbContext _context = new MgDbContext();
 //_context.Database.EnsureDeleted();
 //_context.Database.EnsureCreated();
-
 //var user = _context.Users.FirstOrDefault();
 //Console.WriteLine($"ilk user : {user!.Name}");
 
+//DBCreate();
+
+#region ChangeTracking
+
+//UserMg user = new UserMg()
+//{
+//	Name = "Fourth User",
+//	Age = 33
+//};
+
+//using (var db = new MgDbContext())
+//{
+//	var entry = db.Entry(user);
+//	db.Entry(user).State = EntityState.Added;
+//	entry = db.Entry(user);
+//	db.SaveChanges();
+
+//	user.Name = "Fourth User Updated";
+//	entry = db.Entry(user);
+//	db.SaveChanges();
+//}
+
+
+
+//using (var db = new MgDbContext())
+//{
+//	// var user = db.Users.FirstOrDefault();
+//	var user = db.Users.AsNoTracking().FirstOrDefault();
+
+//	var entries = db.ChangeTracker.Entries();
+//	foreach ( var entry in entries)
+//	{
+//		Console.WriteLine(entry);
+//	}
+
+//}
+
+//Console.WriteLine($"{user.Id}'li user eklenmiştir."); 
+#endregion
+
 Console.ReadLine();
+
+
+
+void DBCreate()
+{
+	using var db = new MgDbContext();
+	db.Database.EnsureDeleted();
+	db.Database.EnsureCreated();
+}
---
+++ EFCore8.ConsoleApp/Repository/UserMgCommands.cs
@@ -0,0 +1,65 @@
+﻿using EFCore8.ConsoleApp.Data;
+using EFCore8.ConsoleApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore8.ConsoleApp.Repository
+{
+	public class UserMgCommands
+	{
+		private readonly MgDbContext _context;
+
+		public UserMgCommands(MgDbContext context)
+		{
+			_context = context;
+		}
+
+		public void AddUser(UserMg user)
+		{
+			using (_context)
+			{
+				//_context.Users.Add(user);
+				_context.Entry(user).State = EntityState.Added;
+				_context.SaveChanges();
+			}
+		}
+
+		public void UpdateUser(UserMg user)
+		{
+			using (_context)
+			{
+				UserMg? updatedUser = _context.Users.FirstOrDefault(u => u.Name == user.Name);
+
+				if (updatedUser != null) 
+				{
+					updatedUser.Name = user.Name;
+					updatedUser.Age = user.Age;
+
+					_context.SaveChanges();
+				}
+
+				_context.Users.Update(user);
+				//_context.Entry(user).State = EntityState.Modified;
+				_context.SaveChanges();
+			}
+		}
+
+		public void DeleteUser(UserMg user)
+		{
+			using (_context)
+			{
+				UserMg? deletedUser = _context.Users.FirstOrDefault(u => u.Name == user.Name);
+
+				if (deletedUser != null)
+				{
+					_context.Users.Remove(deletedUser);
+					_context.SaveChanges();
+				}
+			}
+		}
+	}
+}
